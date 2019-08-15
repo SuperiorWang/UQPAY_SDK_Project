7e92fd2fd0bb64fab449eac92d39739036678924
cqwang <wangchaoqun@uqpay.com>
2019-08-15 17:05:31 +0800

    Init

---
+++ Public/UQPAY_SDK.h
@@ -0,0 +1,22 @@
+//
+//  UQPAY_SDK.h
+//  UQPAY_SDK
+//
+//  Created by uqpay on 2019/8/14.
+//  Copyright © 2019 优钱付. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UQPAY_SDK.
+FOUNDATION_EXPORT double UQPAY_SDKVersionNumber;
+
+//! Project version string for UQPAY_SDK.
+FOUNDATION_EXPORT const unsigned char UQPAY_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UQPAY_SDK/PublicHeader.h>
+
+#import <UQPAY_SDK/UQPayApi.h>
+#import <UQPAY_SDK/OrderInfo.h>
+#import <UQPAY_SDK/EnvEnum.h>
+#import <UQPAY_SDK/ScenesEnum.h>
